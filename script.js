94a542ccca096f80c1c0af18251f51302edd47e6
DREF Zinedine <Zinedinedref20@gmail.com>
2024-08-22 11:21:49 +0200

    commit1

---
+++ script.js
@@ -0,0 +1,10 @@
+// Définition de la fonction min avec deux paramètres a et b
+function min(a, b) {
+    // Si a est inférieur à b, retourne a, sinon retourne b
+    return a < b ? a : b;
+  }
+  
+
+  console.log(min(0, 10));
+  console.log(min(0, -10));
+  
